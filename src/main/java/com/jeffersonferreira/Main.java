aa55255e965014864eb2d69f8aa87a98f0dc03d4
Jefferson Ferreira <jefferson14489@gmail.com>
2024-06-05 16:57:15 -0300

    Criação de projeto para estudar controle de fluxo em um sistema de processo seletivo

--- src/main/java/com/jeffersonferreira/Main.java
+++ src/main/java/com/jeffersonferreira/Main.java
@@ -2,8 +2,6 @@ package com.jeffersonferreira;
 
 import com.jeffersonferreira.candidatura.ProcessoSeletivo;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
